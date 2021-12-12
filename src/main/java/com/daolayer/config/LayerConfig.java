6bd95723b1bddde02071024b3e1a09fc6a872e92
Alexey Khmelev <a.khmelev91@gmail.com>
2021-12-12 19:58:57 +1000

    security

---
+++ src/main/java/com/daolayer/config/LayerConfig.java
@@ -0,0 +1,26 @@
+package com.daolayer.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class LayerConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        auth.inMemoryAuthentication().withUser("Alex").password(encoder.encode("1234")).roles("ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.formLogin()
+                .and()
+                .authorizeRequests().antMatchers("/persons/by-age").permitAll()
+                .and()
+                .authorizeRequests().anyRequest().authenticated();
+    }
+}
